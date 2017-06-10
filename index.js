c63a264ceaff92617b462193570eb3c279990eb2
Chad Kirby <chad@patentnavigation.com>
2017-06-10 13:57:16 -0700

    rewrite asset paths inside srcset lists

--- index.js
+++ index.js
@@ -93,20 +93,20 @@ AssetRewrite.prototype.rewriteAssetPath = function (string, assetPath, replaceme
    *
    * Uses a regular expression to find assets in html tags, css backgrounds, handlebars pre-compiled templates, etc.
    *
-   * ["\'(=] - Match one of "'(= exactly one time
+   * ["\'(=,] - Match one of "'(=, exactly one time - the comma matches the end of the previous path inside a srcset
    * \\s* - Any amount of white space
    * ( - Starts the first capture group
-   * [^"\'()=]* - Do not match any of ^"'()= 0 or more times
-   * [^"\'()\\>=]* - Do not match any of ^"'()\>= 0 or more times - Explicitly add \ here because of handlebars compilation
+   * [^"\'()=,]* - Do not match any of ^"'()=, 0 or more times
+   * [^"\'()\\>=,]* - Do not match any of ^"'()\>= 0 or more times - Explicitly add \ here because of handlebars compilation
    * ) - End first capture group
    * (\\?[^"\')> ]*)? - Allow for query parameters to be present after the URL of an asset
    * \\s* - Any amount of white space
    * \\\\* - Allow any amount of \ - For handlebars compilation (includes \\\)
    * \\s* - Any amount of white space
-   * ["\')> ] - Match one of "'( > exactly one time
+   * ["\')> ,] - Match one of "'( >, exactly one time
    */
 
-  var re = new RegExp('["\'(=]\\s*([^"\'()=]*' + escapeRegExp(assetPath) + '[^"\'()\\>=]*)(\\?[^"\')> ]*)?\\s*\\\\*\\s*["\')> ]', 'g');
+  var re = new RegExp('["\'(=,]\\s*([^"\'()=,]*' + escapeRegExp(assetPath) + '[^"\'()\\>=,]*)(\\?[^"\')> ]*)?\\s*\\\\*\\s*["\')> ,]', 'g');
   var match = null;
   /*
    * This is to ignore matches that should not be changed
